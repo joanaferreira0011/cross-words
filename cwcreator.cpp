b850e3247745efec684e6f0346c3f952612d3477
Joana Ferreira <joanaferreira0011@gmail.com>
2018-05-19 14:22:09 +0100

    call for suggestions

--- cwcreator.cpp
+++ cwcreator.cpp
@@ -104,7 +104,8 @@ void cwcreator::update_board(Board boardname, Dictionary dictionaryname)
 				boardname.remove_word(position); // Remove Word
 			else if (word == "?")
 			{
-				cout << "INCOMPLETO" << endl;
+				vector<string> thewords = boardname.get_wildcard(position);
+				dictionaryname.print_matches(thewords);
 			}
 			else if (dictionaryname.validword(word)) // Is valid?
 				boardname.addword(position, word); // Add Word
